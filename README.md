cff6b079cb609852a6f158cf2265c98e84acf772
Neha Srinivasa <neha.srinivasa123@gmail.com>
2025-11-27 19:17:52 -0500

    Initial upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# e-planting shopping site
+*Still in progress*
+Uses front-end stack converted to React to create an online plant shopping site
---
+++ src/CartSlice.jsx
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const CartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    items: [], // Initialize items as an empty array
+  },
+  reducers: {
+    addItem: (state, action) => {
+    
+    },
+    removeItem: (state, action) => {
+    },
+    updateQuantity: (state, action) => {
+
+    
+    },
+  },
+});
+
+const [addedToCart, setAddedToCart] = useState({});
+
+const handleAddToCart = (product) => {
+  dispatch(addItem(product));
+  setAddedToCart((prevState) => ({
+     ...prevState,
+     [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+   }));
+};
+export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
+
+export default CartSlice.reducer;
